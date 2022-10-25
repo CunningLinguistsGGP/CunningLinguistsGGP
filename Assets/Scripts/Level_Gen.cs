585df1170c1203ea1b73952b34b92b1940c57def
Aditya Varma <80969672+AdityaVarmaAV@users.noreply.github.com>
2022-10-25 17:27:28 +0100

    UI fade Completed

--- Assets/Scripts/Level_Gen.cs
+++ Assets/Scripts/Level_Gen.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level_Gen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Level_Gen instance;
+
+    GameObject Previous_Level;
+
+    GameObject Present_Level;
+
+    GameObject Player_SpwanPoint;
+
+    [SerializeField]GameObject Black_Fade;
+
+    [SerializeField] float Fade_speed;
+
+    private void Start()
     {
-        
+        instance = this;
+
+        Fade_speed = 1f; // Default. 1 second
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Next_level()
     {
-        
+        StartCoroutine(start_fade(true));
+    }
+
+    public IEnumerator start_fade(bool status)
+    {
+        for(var t = 0f; t< 1; t += Time.deltaTime / Fade_speed)
+        {
+            var tempColor = Black_Fade.GetComponent<Image>().color;
+
+            Black_Fade.SetActive(true);
+
+            switch (status)
+            {
+                case true:
+                    tempColor.a = Mathf.Lerp(0f, 1f, t);
+                    Black_Fade.GetComponent<Image>().color = tempColor;
+                    Debug.Log(tempColor.a);
+                    break;
+                case false:
+                    tempColor.a = Mathf.Lerp(1f, 0f, t);
+                    Black_Fade.GetComponent<Image>().color = tempColor;
+                    break;
+            }
+
+            
+
+            yield return null;
+        }
+
+        Black_Fade.SetActive(status == true ? true : false);
+
+        // yield return new WaitForSeconds(1);
     }
 }
