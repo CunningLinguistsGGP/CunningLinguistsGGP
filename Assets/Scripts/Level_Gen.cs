aeed0ae9be3b0b975f17e0d94cbe319fb28aa4be
Aditya Varma <80969672+AdityaVarmaAV@users.noreply.github.com>
2022-10-25 18:03:12 +0100

    Level scripting done

--- Assets/Scripts/Level_Gen.cs
+++ Assets/Scripts/Level_Gen.cs
@@ -7,9 +7,17 @@ public class Level_Gen : MonoBehaviour
 {
     public static Level_Gen instance;
 
+    [SerializeField] GameObject[] Levels;
+
+    [SerializeField] GameObject Player;
+
+    List<int> level_selection;
+
     GameObject Previous_Level;
 
     GameObject Present_Level;
+
+    [SerializeField]GameObject Level_SpwanPoint;
 
     GameObject Player_SpwanPoint;
 
@@ -17,17 +25,42 @@ public class Level_Gen : MonoBehaviour
 
     [SerializeField] float Fade_speed;
 
+    int random;
+
     private void Start()
     {
         instance = this;
 
         Fade_speed = 1f; // Default. 1 second
+
+        level_selection = new List<int>();
+
+        for(int i=0; i< Levels.Length; i++)
+        {
+            level_selection.Add(i);
+        }
     }
 
 
     public void Next_level()
     {
         StartCoroutine(start_fade(true));
+
+        if (Present_Level != null) { Previous_Level = Present_Level; Previous_Level.gameObject.SetActive(false); }
+
+        random = Random.Range(0, level_selection.Count);
+
+        level_selection.Remove(level_selection[random]);
+
+        Present_Level = Instantiate(Levels[level_selection[random]], Level_SpwanPoint.transform);
+
+        Player_SpwanPoint = Present_Level.transform.Find("PlayerSpwanPoint").gameObject;
+
+        Player.transform.position = Player_SpwanPoint.transform.position;
+
+
+
+        StartCoroutine(start_fade(false));
     }
 
     public IEnumerator start_fade(bool status)
