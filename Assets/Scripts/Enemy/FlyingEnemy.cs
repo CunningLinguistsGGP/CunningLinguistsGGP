97640b26f532e40db0aca643f815f679fed9a674
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-10-27 22:42:49 +0100

    Flying enemy now does ranged damage need to fix it because at the moment it acts like a hit scan weapon for some reason

--- Assets/Scripts/Enemy/FlyingEnemy.cs
+++ Assets/Scripts/Enemy/FlyingEnemy.cs
@@ -6,13 +6,8 @@ using UnityEngine.AI;
 
 public class FlyingEnemy : MonoBehaviour
 {
-    //public Rigidbody projectile;
-    //public float range = 50.0f;
-    //public float bulletImpulse = 20.0f;
-
     public float radius;
     public float enemyCooldown;
-    public float damage;
 
     private float timer;
     private bool playerInRange;
@@ -24,7 +19,6 @@ public class FlyingEnemy : MonoBehaviour
     private new Transform camera;
 
     [SerializeField] private GameObject projectile;
-    [SerializeField] private bool infiniteAmmo = true;
     [SerializeField] private float shootCooldown = 1.0f, shotSpeed = 10.0f, targetRange = 50.0f;
     [SerializeField] private Transform projectileSpawn;
     
@@ -34,8 +28,6 @@ public class FlyingEnemy : MonoBehaviour
     private bool canShoot = true;
     private float lastShotTime = 0.0f;
 
-
-  
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -83,39 +75,34 @@ public class FlyingEnemy : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
-    
+
     void Shoot()
     {
-        Vector3 forward = transform.forward;
-        Vector3 rayDir = forward;
+        Vector3 rayDir = transform.position;
 
         Ray ray = new Ray(transform.position, rayDir);
         RaycastHit hit;
-
-        if(mzzlFlash!=null)
-            mzzlFlash.Play();
-        if(audioShot!=null)
-        {
-            audioShot.Play();
-            audioShot.SetScheduledEndTime(AudioSettings.dspTime + shootCooldown);
-        }
         
         Vector3 targetPos = ray.GetPoint(targetRange);
         if (Physics.Raycast(transform.position, rayDir, out hit))
         {
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null)
+            if (playerHealth.currentHealth > 0)
             {
-                if (playerHealth.currentHealth > 0)
+                targetPos = hit.point;
+                
+                if(mzzlFlash!=null)
+                    mzzlFlash.Play();
+                if(audioShot!=null)
                 {
-                    targetPos = hit.point;
-                    playerHealth.currentHealth -= damage;
+                    audioShot.Play();
+                    audioShot.SetScheduledEndTime(AudioSettings.dspTime + shootCooldown);
                 }
+                
+                GameObject newProjectile = Instantiate(projectile, projectileSpawn.position, Quaternion.identity);
+                newProjectile.GetComponent<Rigidbody>().velocity = rayDir.normalized * shotSpeed;
+                //Destroy(newProjectile, 2.0f);
             }
         }
-        GameObject newProjectile = Instantiate(projectile, projectileSpawn.position, Quaternion.identity);
-        newProjectile.GetComponent<Rigidbody>().velocity = rayDir.normalized * shotSpeed;
-        Destroy(newProjectile, 2.0f);
         
         lastShotTime = Time.time;
     }
