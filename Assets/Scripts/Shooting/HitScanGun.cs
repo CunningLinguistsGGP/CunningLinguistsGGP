ae324273a876b795704c2e03948dd0e2a520b460
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-11-21 21:00:48 +0000

    Fixed collison issues with enemies as well as with camera

--- Assets/Scripts/Shooting/HitScanGun.cs
+++ Assets/Scripts/Shooting/HitScanGun.cs
@@ -7,7 +7,9 @@ public class HitScanGun : MonoBehaviour
 {
     [SerializeField] private float damage = 10.0f;
     [SerializeField] private float range = 100.0f;
+
     [SerializeField] private float shots = 1;
+
     //[SerializeField] private float armorPen = 0.0f;
     [SerializeField] private float coneAngle = 3.0f;
     [SerializeField] private ParticleSystem mzzlFlash;
@@ -22,6 +24,7 @@ public class HitScanGun : MonoBehaviour
     [SerializeField] private GameObject DamageTextPrefab;
 
     private float lastShotTime = 0.0f;
+
     void Start()
     {
         audioShot = GetComponent<AudioSource>();
@@ -43,6 +46,8 @@ public class HitScanGun : MonoBehaviour
         Vector3 up = aimCam.transform.up;
         Vector3 right = aimCam.transform.right;
         Vector3 rayDir = forward;
+        int layerMask = 1 << 5;
+        layerMask = ~layerMask;
 
         RaycastHit hit;
         if(lastShotTime+shootCooldown<Time.time)
@@ -52,7 +57,7 @@ public class HitScanGun : MonoBehaviour
             audioShot.SetScheduledEndTime(AudioSettings.dspTime+shootCooldown);
             if (shots == 1)
             {
-                if (Physics.Raycast(aimCam.transform.position, rayDir, out hit, range))
+                if (Physics.Raycast(aimCam.transform.position, rayDir, out hit, range, layerMask, QueryTriggerInteraction.Ignore))
                 {
                     Target target = hit.transform.GetComponent<Target>();
                     if (target != null)
@@ -68,7 +73,7 @@ public class HitScanGun : MonoBehaviour
                 for (int i = 0; i < shots; i++)
                 {
                     rayDir = Vector3.Normalize(forward+shotDeviation * (Random.Range(-1.0f, 1.0f) * right + Random.Range(-1.0f, 1.0f) * up));
-                    if (Physics.Raycast(aimCam.transform.position, rayDir, out hit, range))
+                    if (Physics.Raycast(aimCam.transform.position, rayDir, out hit, range, layerMask, QueryTriggerInteraction.Ignore))
                     {
                         Target target = hit.transform.GetComponent<Target>();
                         if (target != null)
