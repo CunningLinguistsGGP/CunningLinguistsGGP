a17ae5cf09db277f86e529fab1ce56b1a088f1bd
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-11-24 13:25:18 +0000

    Added a shield enemy

---
+++ Assets/Scripts/Enemy/ShieldEnemy.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShieldEnemy : MonoBehaviour
+{
+    public float enemyCooldown;
+
+    private float timer;
+    private bool playerInRange;
+
+    private NavMeshAgent agent;
+    private GameObject player;
+    private PlayerScript playerHealth;
+    private MeshRenderer glow;
+    
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float shotSpeed = 10.0f;
+    [SerializeField] private Transform projectileSpawn;
+    
+    [SerializeField] private ParticleSystem mzzlFlash;
+    [SerializeField] AudioSource audioShot;
+
+    //Grapple
+    private bool stunned;
+    private float stunTime = 2f;
+    private Target enemy;
+
+    private void Start()
+    {
+        glow = GetComponent<MeshRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerScript>();
+        enemy = GetComponent<Target>();
+
+
+    }
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (stunned)
+        {
+            agent.isStopped = true;
+
+            enemy.SetDoubleDamage(true);
+
+            stunTime -= Time.deltaTime;
+
+            if (stunTime <= 0f)
+            {
+                stunned = false;
+                agent.isStopped = false;
+                enemy.SetDoubleDamage(false);
+                stunTime = 2f;
+            }
+        }
+        else
+        {
+            if (timer >= enemyCooldown && playerInRange)
+            {
+                Shoot();
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            glow.material.EnableKeyword("_EMISSION");
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            glow.material.DisableKeyword("_EMISSION");
+            playerInRange = false;
+        }
+    }
+    
+    void Shoot()
+    {
+        timer = Random.Range(0.0f, 2.0f);
+        
+        if (playerHealth.currentHealth > 0)
+        {
+            if (mzzlFlash != null)
+            {
+                mzzlFlash.Play();
+            }
+            
+            if(audioShot != null)
+            {
+                audioShot.Play();
+                audioShot.SetScheduledEndTime(AudioSettings.dspTime + enemyCooldown);
+            }
+                
+            GameObject newProjectile = Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+            newProjectile.GetComponent<Rigidbody>().velocity = (player.transform.position - projectileSpawn.position).normalized * shotSpeed;
+            Destroy(newProjectile, 2.0f);
+        }
+    }
+
+    public bool SetStunned(bool stun)
+    {
+        return stunned = stun;
+    }
+}
