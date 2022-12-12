e3e1575c887863bb13c512f32ddc6b44a1204a36
Sam Watling <samwatling123@hotmail.co.uk>
2022-12-12 12:02:53 +0000

    somehow my shit has been fucked again

--- Assets/Scripts/Shooting/AimAssist.cs
+++ Assets/Scripts/Shooting/AimAssist.cs
@@ -7,7 +7,7 @@ public class AimAssist : MonoBehaviour
     [SerializeField] private List<GameObject> aimBubbles;
     [SerializeField] private float bubbleRadius;
 
-    [SerializeField] private float aimAssistSpeed;
+    [SerializeField] private float aimAssistSpeed = 0.1f;
 
     [SerializeField] private Camera aimCam;
 
@@ -17,10 +17,10 @@ public class AimAssist : MonoBehaviour
     void Start()
     {
         aimBubbles.AddRange(GameObject.FindGameObjectsWithTag("AimAssist"));
-        //for (int i = 0; i < aimBubbles.Count; i++)
-        //{
-        //    aimBubbles[i].GetComponent<SphereCollider>().radius = bubbleRadius;
-        //}
+        for (int i = 0; i < aimBubbles.Count; i++)
+        {
+            aimBubbles[i].GetComponent<SphereCollider>().radius = bubbleRadius;
+        }
     }
 
     // Update is called once per frame
@@ -33,19 +33,13 @@ public class AimAssist : MonoBehaviour
         Ray ray = new Ray(aimCam.transform.position, rayDir);
         RaycastHit hit;
 
-        Debug.Log("here");
-
+        int layerMask = 1 << 9;
         Vector3 targetPos = ray.GetPoint(targetRange);
-        if (Physics.Raycast(ray, out hit, targetRange, LayerMask.NameToLayer("AimAssist"))) ;
+        if (Physics.Raycast(ray, out hit, targetRange, layerMask))
         {
-            Debug.Log("assisting");
-            //aimCam.transform.rotation = Quaternion.Slerp(aimCam.transform.rotation, Quaternion.LookRotation(hit.transform.position - aimCam.transform.position), aimAssistSpeed * Time.deltaTime);
-            aimCam.transform.rotation = Quaternion.identity;
-        }
-    }
+            aimCam.transform.rotation = Quaternion.Lerp(aimCam.transform.rotation, Quaternion.LookRotation(hit.transform.position - aimCam.transform.position), aimAssistSpeed);
 
-    public float GetRadius()
-    {
-        return bubbleRadius;
+        }
+
     }
 }
