a0b0526ea5c27f4caf8a10ed28c3fbae4fd7f571
Sam Watling <samwatling123@hotmail.co.uk>
2022-12-12 14:59:09 +0000

    really should work for aimAssist this time

--- Assets/Scripts/Shooting/AimAssist.cs
+++ Assets/Scripts/Shooting/AimAssist.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AimAssist : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> aimBubbles;
+    //[SerializeField] private List<GameObject> aimBubbles;
     [SerializeField] private float bubbleRadius;
 
     [SerializeField] private float aimAssistSpeed = 0.1f;
@@ -16,15 +16,15 @@ public class AimAssist : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        aimBubbles.AddRange(GameObject.FindGameObjectsWithTag("AimAssist"));
-        for (int i = 0; i < aimBubbles.Count; i++)
-        {
-            aimBubbles[i].GetComponent<SphereCollider>().radius = bubbleRadius;
-        }
+        //aimBubbles.AddRange(GameObject.FindGameObjectsWithTag("AimAssist"));
+        //for (int i = 0; i < aimBubbles.Count; i++)
+        //{
+        //    aimBubbles[i].GetComponent<SphereCollider>().radius = bubbleRadius;
+        //}
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
 
         Vector3 forward = aimCam.transform.forward;
@@ -37,11 +37,13 @@ public class AimAssist : MonoBehaviour
         Vector3 targetPos = ray.GetPoint(targetRange);
         if (Physics.Raycast(ray, out hit, targetRange, layerMask))
         {
+            Debug.Log("Aiming");
             desiredRot = Quaternion.Lerp(desiredRot, Quaternion.LookRotation(hit.transform.position - aimCam.transform.position), aimAssistSpeed);
             aimCam.transform.rotation = Quaternion.Lerp(aimCam.transform.rotation, Quaternion.LookRotation(hit.transform.position - aimCam.transform.position), aimAssistSpeed);
+            //aimCam.transform.rotation = desiredRot;
+            
+            //aimCam.transform.parent.transform.Rotate(Vector3.up *-1* desiredRot.eulerAngles.y);///
             //aimCam.transform.localRotation = Quaternion.Euler(desiredRot.eulerAngles.x, 0f, 0f);
-
-            //aimCam.transform.parent.transform.Rotate(Vector3.up *-1* desiredRot.eulerAngles.y);
         }
 
     }
